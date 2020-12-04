72b9e0843cc1efa817b1c99515af3028bda83c5e
nullmage <jsky.johnson@gmail.com>
2020-12-04 13:04:04 -0700

    Adds 404 page and basic thumb

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Container, Header } from 'semantic-ui-react'
+import 'assets/css/pages/404.scss'
 
 export const page404 = () => {
 	return (
 		<div>
-			This is a 404 page...
-		</div>
+      <Header as="h2" className="fourofour_header">
+        404<span className="dot">.</span>
+        <div className="line" />
+      </Header>
+      <div className="fourofour_clone">404</div>
+
+			<Container>
+				<p className="page_description">The page you are looking for couldn't be found! Something might have been misspelled, removed, or maybe the internet is broken.</p>
+
+				<a className="backtohomepage" href="/">Back to the homepage?</a>
+
+			</Container>
+    </div>
 	)
 }
