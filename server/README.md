9e13589fe359963e954a484787740c37b6218852
nullmage <jsky.johnson@gmail.com>
2020-12-08 01:33:52 -0700

    Adds basic CRUD example to backend server

---
+++ server/README.md
@@ -0,0 +1,7 @@
+This is a simple backend boilerplate for apollo graphql server on typeorm
+
+follow .env.example for needed env variables
+
+`yarn watch` to start tsc -w watch and compile to `/dist`, 
+
+`yarn dev` to use nodemon, note this will run `dist/index.js`, 
---
+++ server/src/index.ts
@@ -0,0 +1,44 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+
+import { buildSchema } from "type-graphql";
+import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
+import { MovieResolver } from "./resolvers/MovieResolver";
+
+require("dotenv").config();
+
+const startServer = async () => {
+  const app = express();
+
+  let db_port = parseInt(process.env.db_port!);
+  await createConnection({
+    type: "postgres",
+    host: process.env.db_host,
+    port: db_port,
+    username: process.env.db_username,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    entities: ["dist/entity/**/*.js"],
+    migrations: ["dist/migration/**/*.js"],
+    subscribers: ["dist/subscriber/**/*.js"],
+    synchronize: true,
+    logging: true,
+  });
+
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [MovieResolver],
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  server.applyMiddleware({ app, cors: false });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+};
+
+startServer();
